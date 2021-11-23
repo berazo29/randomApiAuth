c027489063fe1b864fc27da1446cda0f661e204e
Bryan Erazo <bryanerazo29@gmail.com>
2021-11-23 17:56:04 -0500

    Add basic routes

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/login', (req, res) => {
+  res.send('I am login')
+})
+
+app.post('/register', (req, res) => {
+  res.send('I am register')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
