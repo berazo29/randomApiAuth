5fa42f0a0485d59a0b2acef26481911fdd36f796
viche <chente.13.garcia@gmail.com>
2021-11-23 19:07:38 -0500

    Working on login

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const session = require('express-session')
 const express = require('express')
 const app = express()
 const port = 3000
@@ -7,8 +7,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+)
+
 app.post('/login', (req, res) => {
-  res.send('I am login')
+  let {username, password} = req.body;
+  if (username === null || username.length === 0) {
+    res.send("Invalid username")
+  }
+  if (password === null || password.length === 0) {
+    res.send("Invalid password")
+  } 
+  res.send('Welcome')
 })
 
 app.post('/register', (req, res) => {
