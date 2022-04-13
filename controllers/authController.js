63fedb066dec6b65f70cd70bc77e3721fc3ab4b9
Bryan Erazo <bryanerazo29@gmail.com>
2022-04-12 23:38:40 -0400

    Refactor auth register function into the auth module.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const { db } = require('../models/db')
 const bcrypt = require('bcryptjs')
+const validator = require('validator')
+const salt = bcrypt.genSaltSync(Number(process.env.BCRYPT_SALT))
 
 const redirectHome = (req, res, next) => {
   if (req.session.userId) {
@@ -54,4 +56,50 @@ const login = (req, res) => {
   })
 }
 
-module.exports = { redirectLogin, redirectHome, logout, login }
+const register = (req, res) => {
+  const { email, password, password2 } = req.body
+  const errors = []
+  if (!email) {
+    errors.push('Email cannot be empty.')
+  }
+  if (!password) {
+    errors.push('Password cannot be empty.')
+  }
+  if (password !== password2) {
+    errors.push('Passwords do not match.')
+  }
+  if (errors.length !== 0) {
+    res.render('pages/register', { title: 'register', errors: errors, email: email })
+    return
+  }
+  if (!validator.isEmail(email)) {
+    errors.push('Email is not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    errors.push('Password does not meet security checks.')
+  }
+  if (errors.length !== 0) {
+    res.render('pages/register', { title: 'register', errors: errors, email: email })
+    return
+  }
+
+  const hash = bcrypt.hashSync(password, salt)
+  const sql1 = 'SELECT * FROM users WHERE email = ?'
+  const sql2 = 'INSERT INTO users (email, password) VALUES (?, ?)'
+  const values = [email, hash]
+  db.query(sql1, values[0], (err, results) => {
+    if (err) throw err
+    if (results.length !== 0) {
+      errors.push('Email already taken.')
+      res.render('pages/register', { title: 'register', errors: errors })
+      return
+    }
+    db.query(sql2, values, (err, results) => {
+      if (err) throw err
+      req.session.userId = email
+      res.redirect('/')
+    })
+  })
+}
+
+module.exports = { redirectLogin, redirectHome, logout, login, register }
