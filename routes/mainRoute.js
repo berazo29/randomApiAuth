a0369a2df78eb2d97b377da6853d75bd118d9c84
Bryan Erazo <bryanerazo29@gmail.com>
2022-04-28 19:03:24 -0400

    Added routes for legal contracts.

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -4,5 +4,11 @@ const { redirectLogin } = require('../controllers/authController')
 const { getKeys } = require('../controllers/keyGeneratorController')
 
 router.get('/', redirectLogin, getKeys)
+router.get('/our-term-and-conditions', (req, res) => {
+  res.render('pages/legal/term-conditions', { title: 'Term and Conditions' })
+})
+router.get('/our-privacy-policy', (req, res) => {
+  res.render('pages/legal/privacy-policy', { title: 'Privacy Policy' })
+})
 
 module.exports = router
