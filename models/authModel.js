d1a35d8695ecdafc0056b6e5d9992eafa4c400d4
berazo29 <37014465+berazo29@users.noreply.github.com>
2022-07-25 12:36:51 -0400

    feat: added interfaces to interact with the auth model module

--- models/authModel.js
+++ models/authModel.js
@@ -1,5 +1,8 @@
 const { getUser, changeUserPassword } = require('./user')
-const bcrypt = require('bcryptjs')
+const { isTokenValid, createOneTimeLink } = require('./auth/tokens')
+const { hashPassword, authenticatePassword } = require('./auth/hasher')
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 const loginInterface = (email, password, callback) => {
   getUser(email)
@@ -14,6 +17,17 @@ const loginInterface = (email, password, callback) => {
     .catch(error => callback(error))
 }
 
+const verifyTokenInterface = (email, token, callback) => {
+  getUser(email)
+    .then(user => {
+      if (user === '') return false
+      const uniqueSecret = JWT_SECRET + user.password
+      return isTokenValid(token, uniqueSecret)
+    })
+    .then(result => callback(null, result))
+    .catch(error => callback(error))
+}
+
 const changePasswordInterface = (email, newPassword, callback) => {
   getUser(email)
     .then(user => {
@@ -25,11 +39,16 @@ const changePasswordInterface = (email, newPassword, callback) => {
     .catch(error => callback(error))
 }
 
-const authenticatePassword = password => hash => {
-  if (typeof password === 'string' && typeof hash === 'string') {
-    return bcrypt.compareSync(password, hash)
-  }
-  return false
+const sendResetPasswordInterface = (email, callback) => {
+  getUser(email)
+    .then(user => {
+      if (user === '') return false
+      return sendResetEmail(user)
+    })
+    .then(isSent => {
+      callback(null, isSent)
+    })
+    .catch(error => callback(error))
 }
 
 const hash = rounds => password => {
