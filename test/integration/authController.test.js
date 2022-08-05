96bf97d28be42cec272d2d133ba84ddbc0c04875
berazo29 <37014465+berazo29@users.noreply.github.com>
2022-08-05 01:08:44 -0400

    test: added integration test for authController

--- test/integration/authController.test.js
+++ test/integration/authController.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest')
-const server = require('../index')
-const { db, clientRedis } = require('../models/db')
+const server = require('../../index')
+const { db, clientRedis } = require('../../models/db')
 
 describe('Authentication Integrated Test', () => {
   after(function () {
