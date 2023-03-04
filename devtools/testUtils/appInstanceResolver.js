3aaa85886c2070005dc9370c127a4165af49d0c8
berazo29 <37014465+berazo29@users.noreply.github.com>
2023-03-04 15:50:07 -0500

    test: added delay to print correctly the db connection status

--- devtools/testUtils/appInstanceResolver.js
+++ devtools/testUtils/appInstanceResolver.js
@@ -1,9 +1,12 @@
 let app = null
+const sleep = (ms) => new Promise(resolve => { setTimeout(resolve, ms) })
+
 const getApp = async () => {
   if (app) {
     return app
   }
   app = await require('../../index')
+  await sleep(1000)
   return app
 }
 
